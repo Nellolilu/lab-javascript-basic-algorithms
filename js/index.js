f1a2aa42ce24f4cff7f59a8878f3ef80b2395ce8
Nellolilu <nelli.neb@gmail.com>
2021-01-18 13:45:51 +0100

    Please see idea for Bonus 1.2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 // Iteration 1: Names and Input
 
-
 // Iteration 2: Conditionals
 
-
 // Iteration 3: Loops
+
+// bonus 1.2 ;
+let threeParagraphs = `frst par lorem ispum
+secnd par  lorem ispum
+thrd Par lorem ispum`;
+console.log(threeParagraphs);
+function NumberOf() {
+  let words = threeParagraphs.match(/\w+/g).length;
+  let lines = threeParagraphs.split(/\r\n|\r|\n/).length;
+
+  let characters = threeParagraphs.length;
+
+  console.log("Number of words:", words);
+  console.log("Number of lines:", lines);
+  console.log("Number of characters:", characters);
+}
+NumberOf();
